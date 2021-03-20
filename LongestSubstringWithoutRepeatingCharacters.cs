b93430f423e0bc46fd40f7f97b8663cead7eee27
Ingrid Lootsma <ingrid.lootsma@gmail.com>
2021-03-20 16:45:01 +0100

    LongestSubstringWithoutRepeatingCharacters

---
+++ LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var longest = 0;
+            var current = 0;
+
+            var charInput = s.ToCharArray();
+            var charsSeen = new List<char>();
+
+            for (var startIndex = 0; startIndex < charInput.Length; startIndex++)
+            {
+                if (charInput.Length - startIndex < longest)
+                {
+                    break;
+                }
+
+                current = 0;
+                charsSeen = new List<char>();
+
+                for (var charIndex = startIndex; charIndex < charInput.Length; charIndex++)
+                {
+                    var currentChar = charInput[charIndex];
+
+                    if (charsSeen.Contains(currentChar))
+                    {
+                        break;
+                    }
+
+                    current++;
+
+                    charsSeen.Add(currentChar);
+                }
+
+                if (current > longest)
+                {
+                    longest = current;
+                }
+            }
+
+            return longest;
+        }
+    }
+}
