e0fb609c86cef51caf531436030a6ba5bf29736e
Ingrid Lootsma <ingrid.lootsma@gmail.com>
2021-03-20 20:36:49 +0100

    LongestPalindrome

---
+++ LeetCode/LongestPalindromicSubstring.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LongestPalindromicSubstring
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (s.Length == 1)
+            {
+                return s;
+            }
+
+            var longest = s.Substring(0, 1);
+
+            for (var startIndex = 0; startIndex < s.Length; startIndex++)
+            {
+                for (var endIndex = startIndex + longest.Length; endIndex <= s.Length; endIndex++)
+                {
+                    var inputValue = s.Substring(startIndex, endIndex - startIndex);
+
+                    if (inputValue.Length > longest.Length)
+                    {
+                        var len = inputValue.Length;
+                        var half = len-- >> 1;
+                        var found = true;
+                        for (int i = 0; i < half; i++)
+                        {
+                            if (inputValue[i] != inputValue[len - i])
+                            {
+                                found = false;
+                                break;
+                            }
+                        }
+                        if (found)
+                        {
+                            longest = inputValue;
+                        }
+                    }
+                }
+            }
+
+            return longest;
+        }
+    }
+}
---
+++ LeetCodeTest/LongestPalindromicSubstringTest.cs
@@ -0,0 +1,27 @@
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCodeTest
+{
+    public class LongestPalindromicSubstringTest
+    {
+        [Theory]
+        [InlineData("babad", "bab")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("a", "a")]
+        [InlineData("ac", "a")]
+        [InlineData("aacabdkacaa", "aca")]
+        [InlineData("apqjpwedlhmvvpexxnntxheeynxmgzwxhnhfdvziuxnuusymklgcacndoyhqkoahnkyaikohwkmnuphipftmzmihvmoetskioeypwjujvvusaxynzxxdugnebsisrtgeujkqkgwjuplijhluumqcdurovyjsbowmnqndejwkihzbbdyxjunkduyqeihektaknbmkzgnnmgywylulxwyywrvieqfenjeljofkqqqisdjsbfkvqgahxwkfkcucvrbbpyhwkfztjdboavtfynrudneieelwlcezqsuhmllcsadcnoyemsfdlrijoyj", "wyyw")]
+        [InlineData("mwwfjysbkebpdjyabcfkgprtxpwvhglddhmvaprcvrnuxifcrjpdgnktvmggmguiiquibmtviwjsqwtchkqgxqwljouunurcdtoeygdqmijdympcamawnlzsxucbpqtuwkjfqnzvvvigifyvymfhtppqamlgjozvebygkxawcbwtouaankxsjrteeijpuzbsfsjwxejtfrancoekxgfyangvzjkdskhssdjvkvdskjtiybqgsmpxmghvvicmjxqtxdowkjhmlnfcpbtwvtmjhnzntxyfxyinmqzivxkwigkondghzmbioelmepgfttczskvqfejfiibxjcuyevvpawybcvvxtxycrfbcnpvkzryrqujqaqhoagdmofgdcbhvlwgwmsmhomknbanvntspvvhvccedzzngdywuccxrnzbtchisdwsrfdqpcwknwqvalczznilujdrlevncdsyuhnpmheukottewtkuzhookcsvctsqwwdvfjxifpfsqxpmpwospndozcdbfhselfdltmpujlnhfzjcgnbgprvopxklmlgrlbldzpnkhvhkybpgtzipzotrgzkdrqntnuaqyaplcybqyvidwcfcuxinchretgvfaepmgilbrtxgqoddzyjmmupkjqcypdpfhpkhitfegickfszermqhkwmffdizeoprmnlzbjcwfnqyvmhtdekmfhqwaftlyydirjnojbrieutjhymfpflsfemkqsoewbojwluqdckmzixwxufrdpqnwvwpbavosnvjqxqbosctttxvsbmqpnolfmapywtpfaotzmyjwnd", "khvhk")]
+        [InlineData("flsuqzhtcahnyickkgtfnlyzwjuiwqiexthpzvcweqzeqpmqwkydhsfipcdrsjkefehhesubkirhalgnevjugfohwnlhbjfewiunlgmomxkafuuokesvfmcnvseixkkzekuinmcbmttzgsqeqbrtlwyqgiquyylaswlgfflrezaxtjobltcnpjsaslyviviosxorjsfncqirsjpkgajkfpoxxmvsyynbbovieoothpjgncfwcvpkvjcmrcuoronrfjcppbisqbzkgpnycqljpjlgeciaqrnqyxzedzkqpqsszovkgtcgxqgkflpmrikksaupukdvkzbltvefitdegnlmzeirotrfeaueqpzppnsjpspgomyezrlxsqlfcjrkglyvzvqakhtvfmeootbtbwfhqucbnuwznigoyatvkocqmbtqghybwrhmyvvuchjpvjckiryvjfxabezchynfxnpqaeampvaapgmvoylyutymdhvhqfmrlmzkhuhupizqiujpwzarnszrexpvgdmtoxvjygjpmiadzdcxtggwamkbwrkeplesupagievwsaaletcuxtpsxmbmeztcylsjxvhzrqizdmgjfyftpzpgxateopwvynljzffszkzzqgofdlwyknqfruhdkvmvrrjpijcjomnrjjubfccaypkpfokohvkqndptciqqiscvmpozlyyrwobeuazsawtimnawquogrohcrnmexiwvjxgwhmtpykqlcfacuadyhaotmmxevqwarppknoxthsmrrknu", "ljpjl")]
+        public void Test1(string input, string expected)
+        {
+            var container = new LongestPalindromicSubstring();
+            var result = container.LongestPalindrome(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
